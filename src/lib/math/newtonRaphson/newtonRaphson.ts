78d3207c33fcefa733d02853066dfed561efbcb4
Breno Magalhães <breno.magalhaes@vallourec.com>
2021-11-03 15:56:19 -0300

    feat(newtonraphson): add draft for finding cross zero functions

--- src/lib/math/newtonRaphson/newtonRaphson.ts
+++ src/lib/math/newtonRaphson/newtonRaphson.ts
@@ -7,6 +7,7 @@ import {
   EvalFunction,
   MathType,
   parse as parseExpression,
+  sign,
   subtract,
 } from 'mathjs';
 
@@ -17,8 +18,11 @@ const DEFAULT_PRECISION = 5;
 
 export class NewtonRaphson implements INewtonRaphson {
   private readonly funExpression: string;
-  constructor(funExpression: string) {
+  private readonly functionOrder: number;
+
+  constructor(funExpression: string, functionOrder: number) {
     this.funExpression = funExpression;
+    this.functionOrder = functionOrder;
   }
 
   private getEvalFunctions(expression: string, variable: string): EvalFunctions {
@@ -40,7 +44,6 @@ export class NewtonRaphson implements INewtonRaphson {
     const initialH = divide(funResult, derivativeResult);
     let h = initialH;
     let x0 = opts.initialValue as Complex;
-    console.log({ funResult, derivativeResult, initialH, x0 });
     while (abs(h as number) > opts.tolerance) {
       [funResult, derivativeResult] = [
         this.evaluateFunction(fn, x0, opts.variable),
@@ -57,12 +60,33 @@ export class NewtonRaphson implements INewtonRaphson {
     return roots?.map((root) => complex(root.format(precision)));
   }
 
+  private findRootGuesses(fn: EvalFunction, variable: string, minValue: number, maxValue: number, step = 10e-3) {
+    const results = [];
+    const crossZeroValues = [];
+    let pointer = minValue;
+    let index = 0;
+    while (pointer < maxValue) {
+      const result = fn.evaluate({ [variable]: pointer });
+      results.push(result);
+      pointer += step;
+
+      if (index !== 0 && sign(result) !== sign(results[index - 1])) {
+        crossZeroValues.push(pointer);
+      }
+
+      index += 1;
+    }
+    return crossZeroValues;
+  }
+
   findRoots(variable = 's', tolerance = DEFAULT_TOLERANCE): Complex[] {
     const { fun, derivativeFun } = this.getEvalFunctions(this.funExpression, variable);
     const initialValue = -5;
 
     const roots = this.findRoot(fun, derivativeFun, { variable, initialValue, tolerance });
     const rootsWithPrecision = this.setRootsPrecision([roots], DEFAULT_PRECISION);
+    console.log(this.functionOrder);
+    console.log(this.findRootGuesses(fun, variable, -5, 5));
     return rootsWithPrecision;
   }
 }
