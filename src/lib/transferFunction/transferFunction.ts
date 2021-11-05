d79ae036ba2ec957a66b96e1afc0efca57778d97
Breno Magalhães <breno.magalhaes@vallourec.com>
2021-11-05 16:49:15 -0300

    feat(transfer-function): add functions to find root and zeros of a transfer function

--- src/lib/transferFunction/transferFunction.ts
+++ src/lib/transferFunction/transferFunction.ts
@@ -1,18 +1,28 @@
-import { expressionToString } from '../helpers/expressionToString';
+import { complex, Complex } from 'mathjs';
 
-import { Expression, TransferFunctionInput } from './transferFunction.entities';
+import { DurandKerner } from '../math/rootFinding/implementations/durandKerner';
+
+import { ComplexNumber, Expression, TransferFunctionInput } from './transferFunction.entities';
+
+const MAX_ITERATIONS_ROOT = 100;
+const PRECISION = 6;
+const TOLERANCE = 10e-7;
 
 export class TransferFunction {
   private readonly tf: Expression;
+  private readonly poles: Complex[];
+  private readonly zeros: Complex[];
 
   constructor(transferFunctionInput: TransferFunctionInput) {
     this.validateTransferFunctionInput(transferFunctionInput);
     this.tf = {
-      numerator: transferFunctionInput[0],
-      denominator: transferFunctionInput[1],
+      numerator: transferFunctionInput[0].map((coefficient) => this.complexNumberToComplex(coefficient)),
+      denominator: transferFunctionInput[1].map((coefficient) => this.complexNumberToComplex(coefficient)),
     };
-  }
 
+    this.zeros = this.calculateRoots(this.tf.numerator);
+    this.poles = this.calculateRoots(this.tf.denominator);
+  }
   private validateTransferFunctionInput(input: TransferFunctionInput): void {
     if (!input || !input[0] || !input[1]) {
       throw new Error('Please input a valid transfer function');
@@ -25,10 +35,27 @@ export class TransferFunction {
     }
   }
 
+  private complexNumberToComplex(complexNumber: ComplexNumber): Complex {
+    return complex(complexNumber.re, complexNumber.im);
+  }
+
+  private calculateRoots = (coefficients: Complex[]) => {
+    return new DurandKerner(coefficients).findRoots(MAX_ITERATIONS_ROOT, PRECISION, TOLERANCE);
+  };
+
   toString(): string {
-    const numeratorString = expressionToString(this.tf.numerator);
+    /*     const numeratorString = expressionToString(this.tf.numerator as);
     const denominatorString = expressionToString(this.tf.denominator);
-    return `${numeratorString} / ${denominatorString}`;
+    return `${numeratorString} / ${denominatorString}`; */
+    return 'a';
+  }
+
+  pole() {
+    return this.poles;
+  }
+
+  zero() {
+    return this.zeros;
   }
 }
 
