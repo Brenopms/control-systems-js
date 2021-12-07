65a979a0ef571cc2d393042f1631650069ed4b1f
Breno Magalhães <breno.magalhaes@vallourec.com>
2021-12-06 22:33:37 -0300

    feat(polynomialoperations): add multiplication and division operations

--- src/lib/math/polynomialOperations/PolynomialOperations.entities.ts
+++ src/lib/math/polynomialOperations/PolynomialOperations.entities.ts
@@ -1,33 +1,31 @@
 import { Complex } from 'mathjs';
 
-export type NumberOrComplexArray = number[] | Complex[];
-
 export interface IPolynomialOperations {
   /**
    * Add two polynomials
    * @param pol1
    * @param pol2
    */
-  add<T extends NumberOrComplexArray>(pol1: T[], pol2: T[]): T[];
+  add<T extends number | Complex>(pol1: T[], pol2: T[]): T[];
 
   /**
    * Subtract two polynomials
    * @param pol1
    * @param pol2
    */
-  subtract<T extends NumberOrComplexArray>(pol1: T[], pol2: T[]): T[];
+  subtract<T extends number | Complex>(pol1: T[], pol2: T[]): T[];
 
   /**
    * Divide two polynomials
    * @param pol1
    * @param pol2
    */
-  divide<T extends NumberOrComplexArray>(pol1: T[], pol2: T[]): T[];
+  divide<T extends number | Complex>(pol1: T[], pol2: T[]): T[];
 
   /**
    * Multiply two polynomials
    * @param pol1
    * @param pol2
    */
-  multiply<T extends NumberOrComplexArray>(pol1: T[], pol2: T[]): T[];
+  multiply<T extends number | Complex>(pol1: T[], pol2: T[]): T[];
 }
--- src/lib/math/polynomialOperations/implementations/PolynomialOperations.ts
+++ src/lib/math/polynomialOperations/implementations/PolynomialOperations.ts
@@ -1,8 +1,23 @@
-import { add, MathType, subtract } from 'mathjs';
+import { add, Complex, divide, MathType, multiply, subtract } from 'mathjs';
 
 import { range } from '../../../helpers/range';
-import { IPolynomialOperations, NumberOrComplexArray } from '../PolynomialOperations.entities';
+import { IPolynomialOperations } from '../PolynomialOperations.entities';
 
+/**
+ * This is a naive implementation. The computational time is O(mn) for multiplication and division and O(n) for addition and subtraction
+ * There are better algorithms for obtaining the result, especially for multiplication and division
+ *
+ *
+ * "We'll use FFTs to do in O(n log n) time.
+ * This is then used in Schonhage-Strassen integer multiplication algorithm that multiplies two n-bit integers in O(n log n loglog n) time.
+ * We're only going to dopolynomial multiplication."
+ *
+ *
+ * See [here](http://www.cs.cmu.edu/afs/cs/academic/class/15451-s10/www/lectures/lect0423.txt)
+ * and [here](https://www.geeksforgeeks.org/strassens-matrix-multiplication/)
+ * for more details
+ *
+ */
 export class PolynomialOperations implements Partial<IPolynomialOperations> {
   /**
    * Validates polynomials arrays
@@ -20,7 +35,7 @@ export class PolynomialOperations implements Partial<IPolynomialOperations> {
     if (!Array.isArray(pol1) || !Array.isArray(pol2)) {
       return {
         isValid: false,
-        error: 'Invalid Polynomials type. They should be an array of numbers or complex numbers',
+        error: 'Invalid Polynomials type. They should be an array of umbers or complex numbers',
       };
     }
 
@@ -47,7 +62,7 @@ export class PolynomialOperations implements Partial<IPolynomialOperations> {
     return highestOrder;
   }
 
-  add<T extends NumberOrComplexArray>(pol1: T[], pol2: T[]): T[] {
+  add<T extends number | Complex>(pol1: T[], pol2: T[]): T[] {
     const { isValid, error } = this.checkPolynomials(pol1, pol2);
     if (!isValid) {
       throw new Error(error);
@@ -65,7 +80,7 @@ export class PolynomialOperations implements Partial<IPolynomialOperations> {
     return this.reversePolynomial(resultPolynomial);
   }
 
-  subtract<T extends NumberOrComplexArray>(pol1: T[], pol2: T[]): T[] {
+  subtract<T extends number | Complex>(pol1: T[], pol2: T[]): T[] {
     const { isValid, error } = this.checkPolynomials(pol1, pol2);
     if (!isValid) {
       throw new Error(error);
@@ -81,5 +96,48 @@ export class PolynomialOperations implements Partial<IPolynomialOperations> {
     }
 
     return this.reversePolynomial(resultPolynomial);
+  }
+
+  multiply<T extends number | Complex>(pol1: T[], pol2: T[]): T[] {
+    const polSize1 = pol1.length;
+    const polSize2 = pol2.length;
+
+    const orderedPol1 = this.reversePolynomial(pol1);
+    const orderedPol2 = this.reversePolynomial(pol2);
+
+    const newPolSize = polSize1 + polSize2 - 1;
+
+    //Initializing result array with zeros
+    const result: (number | Complex)[] = Array<number>(newPolSize).fill(0);
+
+    for (const i of range(polSize1)) {
+      for (const j of range(polSize2)) {
+        result[i + j] = add(result[i + j], multiply(orderedPol1[i], orderedPol2[j])) as T;
+      }
+    }
+
+    return this.reversePolynomial(result) as T[];
+  }
+
+  divide<T extends number | Complex>(pol1: T[], pol2: T[]): T[] {
+    const polSize1 = pol1.length;
+    const polSize2 = pol2.length;
+
+    const orderedPol1 = this.reversePolynomial(pol1);
+    const orderedPol2 = this.reversePolynomial(pol2);
+
+    const newPolSize = polSize1 + polSize2 - 1;
+
+    //Initializing result array with zeros
+    // TODO: find better typing for this
+    const result: any = Array<number>(newPolSize).fill(0);
+
+    for (const i of range(polSize1)) {
+      for (const j of range(polSize2)) {
+        result[i + j] = add(result[i + j], divide(orderedPol1[i], orderedPol2[j]));
+      }
+    }
+
+    return this.reversePolynomial(result) as T[];
   }
 }
