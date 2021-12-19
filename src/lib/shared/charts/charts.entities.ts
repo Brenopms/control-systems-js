89eb3289065952c8903dae8a1d2b914f3eb6c581
Breno Magalhães <breno.magalhaes@vallourec.com>
2021-12-19 14:44:03 -0300

    feat(charts): add new charts interfaces for output

---
+++ src/lib/shared/charts/charts.entities.ts
@@ -0,0 +1,18 @@
+export interface AxisDetails {
+  label: string;
+  unit?: string;
+}
+
+export interface Trace<T> {
+  x: T[];
+  y: T[];
+}
+
+export interface ChartData<T> {
+  data: Trace<T> | Trace<T>[];
+  details: {
+    name: string;
+    xAxis: AxisDetails;
+    yAxis: AxisDetails;
+  };
+}
