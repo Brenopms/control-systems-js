5467566a15d00f1f105ae944d9601f6e9d62ac36
Brenopms <brenosilva1@gmail.com>
2023-01-06 18:31:16 -0300

    test(bode): add initial tests for bode class

---
+++ src/lib/bode/bode.spec.ts
@@ -0,0 +1,53 @@
+import { complex } from 'mathjs';
+import { beforeEach, describe, expect, it, SpyInstance, vi } from 'vitest';
+
+import { ICalculateTransferFunction } from '../math/calculateTransferFunction/calculateTransferFunction.entities';
+import { CalculateTransferFunction } from '../math/calculateTransferFunction/implementations/calculateTransferFunction';
+import { TransferFunctionExpression } from '../transferFunction/transferFunction.entities';
+
+import { Bode } from './bode';
+import { IBode } from './bode.entities';
+
+describe('Bode Class', () => {
+  let bode: IBode;
+  let calculateTransferFunction: ICalculateTransferFunction;
+  let calculationSpy: SpyInstance<[transferFunction: TransferFunctionExpression, s: math.Complex], math.Complex>;
+
+  beforeEach(() => {
+    calculateTransferFunction = new CalculateTransferFunction();
+    calculationSpy = vi.spyOn(calculateTransferFunction, 'calculateValue');
+    bode = new Bode(calculateTransferFunction);
+  });
+
+  it('Should calculate correctly the magnitude data given a set of frequencies', () => {
+    //  s + 2 / 2 s + 1
+    // f = bodemag(e, 10)
+    // f = 0.5093
+    const transferFunction: TransferFunctionExpression = {
+      numerator: [complex(1, 0), complex(2, 0)],
+      denominator: [complex(2, 0), complex(1, 0)],
+    };
+    calculationSpy.mockReturnValue(complex(0.5093, 0));
+
+    const result = bode.calculatePoints(transferFunction, [10]);
+
+    expect(result.magnitude[0]).toBeDefined();
+    expect(result.magnitude[0].y).toBeCloseTo(-5.8611);
+    expect(result.magnitude[0].x).toBe(10);
+  });
+
+  it('Should calculate correctly the phase data given a set of frequencies', () => {
+    //  s + 2 / 2 s + 1
+    const transferFunction: TransferFunctionExpression = {
+      numerator: [complex(1, 0), complex(2, 0)],
+      denominator: [complex(2, 0), complex(1, 0)],
+    };
+    calculationSpy.mockReturnValue(complex(0.5093, 10));
+
+    const result = bode.calculatePoints(transferFunction, [10]);
+
+    expect(result.magnitude[0]).toBeDefined();
+    expect(result.magnitude[0].y).toBeCloseTo(20.011);
+    expect(result.magnitude[0].x).toBe(10);
+  });
+});
