031e304fb3220a1a7c2663e3edd2fe30093546a9
Brenopms <brenosilva1@gmail.com>
2023-01-07 15:47:09 -0300

    feat(chart): add traceToPoints utils function

--- src/lib/shared/charts/chart.utils.ts
+++ src/lib/shared/charts/chart.utils.ts
@@ -1,6 +1,6 @@
 import { Point, Trace } from './charts.entities';
 
-export const transformPointsToTrace = <T>(points: Point<T>[]): Trace<T> => {
+export const pointsToTrace = <T>(points: Point<T>[]): Trace<T> => {
   const traceX: T[] = [];
   const traceY: T[] = [];
 
@@ -13,4 +13,9 @@ export const transformPointsToTrace = <T>(points: Point<T>[]): Trace<T> => {
     x: traceX,
     y: traceY,
   };
+};
+
+export const traceToPoints = <T>(trace: Trace<T>): Point<T>[] => {
+  const points = trace?.x.map((x, index) => ({ x: x, y: trace?.y?.[index] }));
+  return points;
 };
