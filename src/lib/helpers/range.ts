550a1f8a2f279362dee5e003b4b7899da69afc3a
Brenopms <brenosilva1@gmail.com>
2023-01-16 15:54:54 -0300

    refactor: change range to use boundaryRange fn

--- src/lib/helpers/range.ts
+++ src/lib/helpers/range.ts
@@ -1,7 +1,5 @@
+import { boundaryRange } from './boundaryRange';
+
 export const range = (size: number, step = 1): number[] => {
-  const rangeValues = [];
-  for (let i = 0; i < size; i += step) {
-    rangeValues.push(i);
-  }
-  return rangeValues;
+  return boundaryRange(0, size, step);
 };
