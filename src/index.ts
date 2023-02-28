d5afd7e1ee460e15c492fc3fdd7aa843cf592d5f
Brenopms <brenosilva1@gmail.com>
2023-02-28 15:35:43 -0300

    feat: export public functions

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,76 @@
+import { Bode } from './lib/bode/bode';
+import { IBode } from './lib/bode/bode.entities';
+import { Impulse } from './lib/impulse/impulse';
+import { IImpulse } from './lib/impulse/impulse.entities';
+import { ICalculateTransferFunction } from './lib/math/calculateTransferFunction/calculateTransferFunction.entities';
+import { CalculateTransferFunction } from './lib/math/calculateTransferFunction/implementations/calculateTransferFunction';
+import { Convolution } from './lib/math/convolution/convolution';
+import { IConvolution } from './lib/math/convolution/convolution.entities';
+import { GaverStehfest } from './lib/math/inverseLaplace/implementations/gaverStehfest';
+import { IInverseLaplace } from './lib/math/inverseLaplace/inverseLaplace.entities';
+import { IPolynomialOperations } from './lib/math/polynomialOperations/PolynomialOperations.entities';
 import { PolynomialOperations } from './lib/math/polynomialOperations/implementations/PolynomialOperations';
 import { DurandKerner } from './lib/math/rootFinding/implementations/durandKerner';
+import { IRootFinding } from './lib/math/rootFinding/rootFinding';
+import { RouthHurwitzStability } from './lib/math/stability/implementations/routhHurwitz';
+import { IStability } from './lib/math/stability/stability.entities';
+import { Nyquist } from './lib/nyquist/nyquist';
+import { INyquist } from './lib/nyquist/nyquist.entities';
 import { RootLocus } from './lib/rootLocus/rootLocus';
+import { IRootLocus } from './lib/rootLocus/rootLocus.entities';
+import { Step } from './lib/step/step';
+import { IStep } from './lib/step/step.entities';
 import { TransferFunction } from './lib/transferFunction/transferFunction';
 import { TransferFunctionInput } from './lib/transferFunction/transferFunction.entities';
 
-/* console.log(
-  new TransferFunction([
-    [-1, -2],
-    [1, -2, 3],
-  ]).toString()
-);
- */
+const _calculateTransferFunction: ICalculateTransferFunction = new CalculateTransferFunction();
 
-// TODO: add dependency injection library to manage it
-//TODO: change every array generation to use lazy arrays
-//console.log(new NewtonRaphson('x^2 - 2x + 3', 2).findRoots('x'));
-/* const tf = new TransferFunction({
-  numerator: [
-    { re: 1, im: 1 },
-    { re: -2, im: 0 },
-  ],
-  denominator: [
-    { re: 1, im: 0 },
-    { re: -2, im: 0 },
-    { re: -3, im: 0 },
-  ],
-});
- */
+const _rootFinder: IRootFinding = new DurandKerner();
+const _polynomialOperations: IPolynomialOperations = new PolynomialOperations();
+const _bode: IBode = new Bode(_calculateTransferFunction);
+const _nyquist: INyquist = new Nyquist(_calculateTransferFunction);
 
-//const po = new PolynomialOperations();
+const _rootLocus: IRootLocus = new RootLocus(_polynomialOperations, _rootFinder);
+const _stability: IStability = new RouthHurwitzStability();
+const _inverseLaplace: IInverseLaplace = new GaverStehfest();
+const _convolution: IConvolution = new Convolution(_polynomialOperations);
 
-//console.log(po.multiply([6, 10, 0, 5], [4, 2, 1]));
+const _step: IStep = new Step(_calculateTransferFunction, _inverseLaplace, _convolution);
+const _impulse: IImpulse = new Impulse(_calculateTransferFunction, _inverseLaplace, _convolution);
 
-/**
- *
- *
- * Dependency injection for transfer function class
- *
- *
- *
- */
-
-const rootFinder = new DurandKerner();
-const polynomialOperations = new PolynomialOperations();
-
-const rootLocus = new RootLocus(polynomialOperations, rootFinder);
-
-/**
- * Exposed library function
- * @param transferFunctionInput
- */
-export const transferFunction = (transferFunctionInput: TransferFunctionInput) => {
-  return new TransferFunction(transferFunctionInput, 0, rootFinder, rootLocus);
+const transferFunction = (transferFunctionInput: TransferFunctionInput) => {
+  return new TransferFunction(
+    transferFunctionInput,
+    0,
+    _rootFinder,
+    _rootLocus,
+    _bode,
+    _nyquist,
+    _stability,
+    _step,
+    _impulse
+  );
 };
 
-// const tf = transferFunction({
-//   numerator: [
-//     { re: 1, im: 0 },
-//     { re: 3, im: 0 },
-//   ],
-//   denominator: [
-//     { re: 1, im: 0 },
-//     { re: 3, im: 0 },
-//     { re: 5, im: 0 },
-//     { re: 1, im: 0 },
-//   ],
-// });
+const calculateTransferFunctionValue = _calculateTransferFunction.calculateValue;
+const findRoots = _rootLocus.findRootLocus;
+const bode = _bode.calculatePoints;
+const nyquist = _nyquist.calculatePoints;
+const isStable = _stability.isStable;
+const inverseLaplace = _inverseLaplace.execute;
+const convolute = _convolution.execute;
+const step = _step.calculatePoints;
+const impulse = _impulse.calculatePoints;
 
-const tf2 = transferFunction({
-  numerator: [1, 3],
-  denominator: [1, 3, 5, 1],
-});
-
-console.log(tf2.zero());
-console.log(tf2.pole());
-console.log(tf2.toString());
-console.log(JSON.stringify(tf2.rlocus()));
+export {
+  transferFunction,
+  calculateTransferFunctionValue,
+  findRoots,
+  bode,
+  nyquist,
+  isStable,
+  inverseLaplace,
+  convolute,
+  step,
+  impulse,
+};
