479638d80c77d756bc4b1b4a64ccf90b9768fddf
Brenopms <brenosilva1@gmail.com>
2023-04-02 15:11:17 -0300

    fix(complex): change wrong input

--- src/lib/math/complex/operations/abs.ts
+++ src/lib/math/complex/operations/abs.ts
@@ -3,7 +3,7 @@ import { isComplex } from '../utils/isComplex';
 
 export const abs = (value: number | Complex) => {
   if (isComplex(value)) {
-    const sumOfQuadratic = Math.pow(value.re, 2) + Math.pow(value.re, 2);
+    const sumOfQuadratic = Math.pow(value.re, 2) + Math.pow(value.im, 2);
     const result = Math.sqrt(sumOfQuadratic);
     return result;
   }
