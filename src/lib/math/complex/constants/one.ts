23af397afadcadf61cc1465c178f3bdebca52df4
Brenopms <brenosilva1@gmail.com>
2023-04-02 15:10:18 -0300

    feat: add zero and one constants

---
+++ src/lib/math/complex/constants/one.ts
@@ -0,0 +1,6 @@
+import { Complex } from '../complex';
+
+export const ONE: Complex = {
+  re: 1,
+  im: 0,
+} as const;
---
+++ src/lib/math/complex/constants/zero.ts
@@ -0,0 +1,6 @@
+import { Complex } from '../complex';
+
+export const ZERO: Complex = {
+  re: 0,
+  im: 0,
+} as const;
