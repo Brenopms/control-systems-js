a91ffcab4dce09e79af15017d672eb2525f42004
Brenopms <brenosilva1@gmail.com>
2023-04-04 12:35:57 -0300

    docs(complex): add jsdocs for complex functions

--- src/lib/math/complex/complex.ts
+++ src/lib/math/complex/complex.ts
@@ -1,8 +1,17 @@
+/**
+ * Representation of Complex numbers in rectangular form. Should be somewhat compatible with Complexjs and Mathjs implementations
+ */
 export interface Complex {
   re: number;
   im: number;
 }
 
+/**
+ * Creates instance of Complex number
+ * @param re
+ * @param im
+ * @returns
+ */
 export const complex = (re: number, im: number): Complex => {
   return { re, im };
 };
